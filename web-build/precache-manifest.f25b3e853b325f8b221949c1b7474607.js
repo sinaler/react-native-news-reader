07b403f49b3778b1924523de57a84ad7c29ed881
Sercan Inaler <sercan@gmail.com>
2020-06-06 21:14:10 +0200

    build

---
+++ web-build/precache-manifest.f25b3e853b325f8b221949c1b7474607.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "a867bb6b8e38661d3a5c",
+    "url": "/static/js/app.a325705d.chunk.js"
+  },
+  {
+    "revision": "3636aebd34579cc662c5",
+    "url": "/static/js/runtime~app.4b0f5a04.js"
+  },
+  {
+    "revision": "28e34c7e38a469f63f59",
+    "url": "/static/js/2.50851bd5.chunk.js"
+  },
+  {
+    "revision": "80c23ba63255f1e27d86eb024dba9525",
+    "url": "/static/media/diken.80c23ba6.png"
+  },
+  {
+    "revision": "0c093a818d1957437e82bfd4fcd16a41",
+    "url": "/static/media/duvar.0c093a81.png"
+  },
+  {
+    "revision": "c396b026d007e950338787ea031aa20d",
+    "url": "/static/media/t24.c396b026.png"
+  },
+  {
+    "revision": "4f0e1d367832592df31c2730cf57bc75",
+    "url": "/static/media/sol.4f0e1d36.png"
+  },
+  {
+    "revision": "3177ff0c78d6697dd856b92d2a277329",
+    "url": "/static/media/karinca.3177ff0c.png"
+  },
+  {
+    "revision": "917dd43939e51e9b68bab0d8986c35c1",
+    "url": "/static/media/bianet.917dd439.png"
+  },
+  {
+    "revision": "6bf3a2a260a7de67f5439e30697707f4",
+    "url": "/static/media/evrensel.6bf3a2a2.png"
+  },
+  {
+    "revision": "cacb4a2ac6bfaf7d9417b3c131418ebe",
+    "url": "/static/media/odatv.cacb4a2a.jpg"
+  },
+  {
+    "revision": "5b4ebb0753f8d91043b49593819211fb",
+    "url": "/static/media/aktif-haber.5b4ebb07.jpg"
+  },
+  {
+    "revision": "32dd963bac7336c7859fca178916bcc5",
+    "url": "/static/media/yurt.32dd963b.jpg"
+  },
+  {
+    "revision": "a013cd1a40acae4ae87b58df7304f100",
+    "url": "/static/media/birgun.a013cd1a.png"
+  },
+  {
+    "revision": "5e15d621f77ef8f26906eb275d8167be",
+    "url": "/static/media/sendika.5e15d621.jpg"
+  },
+  {
+    "revision": "b00cbf4405338672d8874eee0b9dfc99",
+    "url": "/static/media/gercekgundem.b00cbf44.png"
+  },
+  {
+    "revision": "217fef1431eccb758ade08ad1197b68c",
+    "url": "/static/media/cumhuriyet.217fef14.png"
+  },
+  {
+    "revision": "ea3ce0c1923fe6e87520f1bfa263d8fd",
+    "url": "/static/media/bbc.ea3ce0c1.png"
+  },
+  {
+    "revision": "53973d4b61e630da4fd859c18a9559ee",
+    "url": "/static/media/acik.53973d4b.png"
+  },
+  {
+    "revision": "2e1638b48c9512fca4f75c2655edbb61",
+    "url": "/static/media/dw.2e1638b4.png"
+  },
+  {
+    "revision": "7ee8c970c4d46b1b98926d3712923ec9",
+    "url": "/static/media/sputnik.7ee8c970.png"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "49a79d66bdea2debf1832bf4d7aca127",
+    "url": "/./fonts/SpaceMono-Regular.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "21c4b367d90ebb55cc6de20b2c6a70d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "853f2c546d292adf477e3b1a1a2e5d19",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4197a9fd61e635cea7da7906870ef364",
+    "url": "/register-service-worker.js"
+  }
+];
